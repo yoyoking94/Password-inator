92e2fe40d5179ec9e6b223075a16b2cae30f4bc4
yoyoking94 <yovish.moonesamy@gmail.com>
2023-10-06 21:32:30 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Password-inator</h1>
+      <h1 className='title'>Password-inator</h1>
       <Preview />
       <Range />
       <Checkbox />
